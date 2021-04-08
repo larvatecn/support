792c930c3a64d864b6a5a031a5c3bc4e856d516e
XuTongle <xutongle@gmail.com>
2021-04-08 12:02:27 +0800

    单页测试

---
+++ tests/SSLCertificateTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This is NOT a freeware, use is subject to license terms
+ * @copyright Copyright (c) 2010-2099 Jinan Larva Information Technology Co., Ltd.
+ * @link http://www.larva.com.cn/
+ * @license http://www.larva.com.cn/license/
+ */
+
+namespace Larva\Support\Tests;
+
+use Larva\Support\HttpClient;
+
+class SSLCertificateTest extends TestCase
+{
+
+    /**
+     * 检测域名
+     */
+    public function testBase()
+    {
+        $cert = HttpClient::getSSLCert('www.larva.com.cn');
+        $this->assertEquals('www.larva.com.cn', $cert->getDomain());
+        $this->assertTrue($cert->isValid('www.larva.com.cn'));
+        $this->assertTrue($cert->isValid('larva.com.cn'));
+        
+    }
+}
